40ad9c8ba2d58dfdd72e13ba600d3e20054c3f73
long <longhoang94>
2018-11-07 22:39:00 +0700

    add interface Repository
    Signed-off-by: long <longhoang94>

---
+++ src/main/java/com/longhoang/repository/Repository.java
@@ -0,0 +1,13 @@
+package com.longhoang.repository;
+
+import java.util.List;
+
+public interface Repository<T> {
+    List<T> findAll();
+
+    T findById(int id);
+
+    void save(T t);
+
+    void remove(int id);
+}
