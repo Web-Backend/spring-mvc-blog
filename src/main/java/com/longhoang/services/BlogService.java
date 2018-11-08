953075c20ae7332b19b5c81e2b2b513b5a3d0512
long <longhoang94>
2018-11-08 08:24:06 +0700

    add function
    Signed-off-by: long <longhoang94>

--- src/main/java/com/longhoang/services/BlogService.java
+++ src/main/java/com/longhoang/services/BlogService.java
@@ -1,4 +1,15 @@
 package com.longhoang.services;
 
+import com.longhoang.models.Blog;
+
+import java.util.List;
+
 public interface BlogService {
+    List<Blog> findAll();
+
+    Blog findById(int id);
+
+    void save(Blog blog);
+
+    void remove(int id);
 }
