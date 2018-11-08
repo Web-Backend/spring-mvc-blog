944f35faa1cadd690c66b79a1fa5341bf17ae5a6
long <longhoang94>
2018-11-08 08:09:03 +0700

    create class Blog
    Signed-off-by: long <longhoang94>

--- src/main/java/com/longhoang/models/Blog.java
+++ src/main/java/com/longhoang/models/Blog.java
@@ -1,4 +1,45 @@
 package com.longhoang.models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Blog")
 public class Blog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String title;
+    private String content;
+
+    public Blog(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Blog() {
+    }
 }
