d5144a69ddb77e71534161bdaedd984a1b1f8823
long <longhoang94>
2018-11-08 09:48:07 +0700

    get list post

--- src/main/java/com/longhoang/controllers/BlogController.java
+++ src/main/java/com/longhoang/controllers/BlogController.java
@@ -1,13 +1,18 @@
 package com.longhoang.controllers;
 
+import com.longhoang.services.BlogService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class BlogController {
-    @GetMapping("/")
+    @Autowired
+    private BlogService blogService;
+
+    @GetMapping("/home")
     public ModelAndView index() {
-        return new ModelAndView("list");
+        return new ModelAndView("list", "blogs", blogService.findAll());
     }
 }
