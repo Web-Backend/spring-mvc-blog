826a7a154e69186048837693580ff3cf3e687539
long <longhoang94>
2018-11-08 20:00:34 +0700

    add function create a post

--- src/main/java/com/longhoang/controllers/BlogController.java
+++ src/main/java/com/longhoang/controllers/BlogController.java
@@ -1,9 +1,13 @@
 package com.longhoang.controllers;
 
+import com.longhoang.models.Blog;
 import com.longhoang.services.BlogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -14,5 +18,21 @@ public class BlogController {
     @GetMapping("/home")
     public ModelAndView index() {
         return new ModelAndView("list", "blogs", blogService.findAll());
+    }
+
+    @GetMapping("/blog/create")
+    public ModelAndView showCreateForm() {
+        ModelAndView modelAndView = new ModelAndView("create");
+        modelAndView.addObject("blog", new Blog());
+        return modelAndView;
+    }
+
+    @PostMapping("/blog/create")
+    public ModelAndView createPost(@ModelAttribute("blog") Blog blog) {
+        blogService.save(blog);
+
+        ModelAndView modelAndView = new ModelAndView("create", "blog", blog);
+        modelAndView.addObject("message", "Created successfully");
+        return modelAndView;
     }
 }
